89ac7142f37f63a6797b185152db1fa4398c52e1
Philip Mostert <philism@Philips-MacBook-Pro.local>
2021-10-19 12:38:16 +0200

    Cleaned Spatial_model

--- Scripts/R/Spatial_model.R
+++ Scripts/R/Spatial_model.R
@@ -1,4 +1,4 @@
-
+##Script to run spatial model + data visualizations
 library(ggplot2)
 library(sf)
 library(sp)
@@ -15,26 +15,33 @@ library(rgeos)
 library(fields)
 library(viridis)
 
+##Read in PO data
+
+##Read in PA data
+
+##Read in habitat + climate data
+
+##Get map of Norway:
+ #Correct projection?
 Projection <- CRS("+proj=longlat +ellps=WGS84")
+
+  #Norway
 norwayfill <- map("world", "norway", fill=TRUE, plot=FALSE, 
                   ylim=c(58,72), xlim=c(4,32))
 IDs <- sapply(strsplit(norwayfill$names, ":"), function(x) x[1])
 norway.poly <- map2SpatialPolygons(norwayfill, IDs = IDs, 
                                    proj4string = Projection)
 
-grid <- makegrid(norway.poly, cellsize = 1)
-grid <- SpatialPoints(grid, proj4string = Projection)
-ggplot()  + gg(grid) + gg(norway.poly)
+#Meshpars <- list(cutoff=0.08, max.edge=c(0.6, 3), offset=c(1,1))
+Meshpars <- list(cutoff=0.08, max.edge=c(1, 3), offset=c(1,1))
 
-spgrdWithin <- SpatialPixels(grid[norway.poly,])
-spgrdWithin <- as(spgrdWithin, "SpatialPolygons")
-ggplot() + gg(spgrdWithin)
 
-Meshpars <- list(cutoff=0.08, max.edge=c(0.6, 3), offset=c(1,1))
-#Meshpars <- list(cutoff=0.08, max.edge=c(1, 3), offset=c(1,1))
+Spatial_data <- organize_data(..., poresp,paresp,trialname,coords, proj = Projection,
+                              speciesname, meshpars = Meshpars, boundary = norway.poly)
 
-Mesh <- MakeSpatialRegion(data=NULL, bdry=norway.poly, meshpars=Meshpars,
-                          proj = Projection)
+Spatial_model <- bru_sdm(spatial_data, spatialcovariates, specieseffects = TRUE,
+                         options = list(control.inla = list(int.strategy = 'eb')))
+
 
 
 
